cfae192c29a93d0efc45cf0c60834c7b2ef6df28
Pablo Donato <pablo.donato@tuta.io>
2022-07-24 13:22:09 +0200

    Manually dynlink (transitive) dependencies, now we get "no implementation available for Stream"

--- theories/Test.v
+++ theories/Test.v
@@ -1,3 +1,10 @@
+Declare ML Module "easy_format".
+Declare ML Module "biniou".
+Declare ML Module "yojson".
+Declare ML Module "re".
+Declare ML Module "atd".
+Declare ML Module "atdgen_runtime".
+Declare ML Module "atdgen".
 Declare ML Module "my_plugin".
 
 CallC.
